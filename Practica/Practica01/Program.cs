ec282f086cab56990a5878b3946a31a8f2a95031
Wifth <alexander.obando.os2206@gmail.com>
2024-10-03 10:52:07 -0500

    Segundo commit

--- Practica/Practica01/Program.cs
+++ Practica/Practica01/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int Suma(int d1, int d2)
+{
+    int x = d1 + d2;
+    return x;
+}
+
+void Imprime (int texto)
+{
+    Console.WriteLine(texto);
+}
+
+int a = 5;
+int b = 6;
+
+//int c = a + b;
+int c = Suma(a, b);
+
+Imprime(c);
+//Console.WriteLine(c);
+
+b = 10;
+
+    int d = a + b;
+
+Console.WriteLine(d);
+
+double z = 100.0;
+float y = (float)100.0;
---
+++ Practica/Practica01/perro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica01
+{
+    internal class perro
+    {
+        public string Nombre { get; set; }
+
+        public static string Ladrar{} {
+        int a = 5;
+        return "Guau";
+    }
+
+    public static void Ladrar(string lad)
+    {
+        Console.WriteLine(lad);
+    }
+}
