7131fa9654254a64fb76d158dafaf1cc81fba774
Wifth <alexander.obando.os2206@gmail.com>
2024-10-03 12:21:43 -0500

    Ejercicio05

--- Teoria/Ejercicio05/Program.cs
+++ Teoria/Ejercicio05/Program.cs
@@ -1,38 +1,44 @@
 ﻿int opcion, x = 0, y = 0;
-do
-{
-    Console.WriteLine("**************************************");
-    Console.WriteLine("\tBienvenidos al sistema \n\t de Funciones Básicas\n");
-    Console.WriteLine("\t1. Suma");
-    Console.WriteLine("\t2. Resta");
-    Console.WriteLine("\t3. Multiplicación");
-    Console.WriteLine("\t4. Division");
-    Console.WriteLine("\t0. Salir");
-    Console.WriteLine(" ");
-    Console.WriteLine("**************************************");
-    Console.Write("\nIngrese una opción: ");
-    opcion = int.Parse(Console.ReadLine());
-    Console.Clear();
+char opcion2;
+do {
+    do
+    {
+        Console.WriteLine("**************************************");
+        Console.WriteLine("\tBienvenidos al sistema \n\t de Funciones Básicas\n");
+        Console.WriteLine("\t1. Suma");
+        Console.WriteLine("\t2. Resta");
+        Console.WriteLine("\t3. Multiplicación");
+        Console.WriteLine("\t4. Division");
+        Console.WriteLine("\t0. Salir");
+        Console.WriteLine(" ");
+        Console.WriteLine("**************************************");
+        Console.Write("\nIngrese una opción: ");
+        opcion = int.Parse(Console.ReadLine());
+        Console.Clear();
 
-} while (opcion < 0 || opcion > 4);
-if (opcion != 0)
-{
-    Console.Write("Ingresar x: ");
-    x = int.Parse(Console.ReadLine());
-    Console.Write("Ingresar y: ");
-    y = int.Parse(Console.ReadLine());
-}
-switch (opcion)
-{
-    case 0: Environment.Exit(0); break;
-    case 1: Console.Write("\nLa suma es: "); suma(x,y); break;
-    case 2: Console.Write("\nLa resta es: "); resta(x, y); break;
-    case 3: Console.Write("\nLa multiplicación es: "); multi(x, y);  break;
-    case 4: Console.Write("\nLa división es: "); divi(x, y);  break;
-}
+    } while (opcion < 0 || opcion > 4);
+    if (opcion != 0)
+    {
+        Console.Write("Ingresar x: ");
+        x = int.Parse(Console.ReadLine());
+        Console.Write("Ingresar y: ");
+        y = int.Parse(Console.ReadLine());
+    }
+    switch (opcion)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1: Console.Write("\nLa suma es: "); suma(x, y); break;
+        case 2: Console.Write("\nLa resta es: "); resta(x, y); break;
+        case 3: Console.Write("\nLa multiplicación es: "); multi(x, y); break;
+        case 4: Console.Write("\nLa división es: "); divi(x, y); break;
+    }
+    Console.Write("\nDeseas continuar? (s): ");
+    opcion2 = char.Parse(Console.ReadLine());
+    Console.Clear();
+} while (opcion2 == 's');
 void suma(int x, int y)
 {
-    Console.WriteLine(x + y);
+    Console.Write(x + y);
 }
 void resta(int x, int y)
 {
@@ -45,8 +51,8 @@ void multi(int x, int y)
 void divi(int x, int y)
 {
     if (y == 0) 
-        Console.WriteLine("Indeterminado");
+        Console.Write("Indeterminado");
     else 
-        Console.WriteLine(x / y);
+        Console.Write(x / y);
 
 }
